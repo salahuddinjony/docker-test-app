904421b51e9bd18b1e67d66740e9a1d83d24ad20
SALAH UDDIN <salahuddin0758@gmail.com>
2026-02-26 15:58:11 +0600

    implement docker using mongo and mongo-express

--- server.js
+++ server.js
@@ -7,36 +7,61 @@ const PORT = 5050;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const MONGO_URL = "mongodb://admin:qwerty@localhost:27017";
+const MONGO_URL =
+  process.env.MONGO_URL ||
+  "mongodb://admin:qwerty@localhost:27027/?authSource=admin";
 const client = new MongoClient(MONGO_URL);
+
+let db;
+
+async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db("salah-db");
+    console.log("Connected successfully to MongoDB");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err.message);
+    console.error("Make sure MongoDB is running on port 27027");
+    process.exit(1);
+  }
+}
 
 //GET all users
 app.get("/getUsers", async (req, res) => {
-    await client.connect(URL);
-    console.log('Connected successfully to server');
-
-    const db = client.db("apnacollege-db");
-    const data = await db.collection('users').find({}).toArray();
-    
-    client.close();
+  try {
+    const data = await db.collection("users").find({}).toArray();
     res.send(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// GET /addUser - redirect to home page with signup form
+app.get("/addUser", (req, res) => {
+  res.redirect("/");
 });
 
 //POST new user
 app.post("/addUser", async (req, res) => {
-    const userObj = req.body;
-    console.log(req.body);
-    await client.connect(URL);
-    console.log('Connected successfully to server');
-
-    const db = client.db("apnacollege-db");
-    const data = await db.collection('users').insertOne(userObj);
+  const userObj = req.body;
+  console.log(req.body);
+  try {
+    const data = await db.collection("users").insertOne(userObj);
     console.log(data);
     console.log("data inserted in DB");
-    client.close();
+    res
+      .status(201)
+      .json({
+        insertedId: data.insertedId,
+        message: "User added successfully",
+      });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-
-app.listen(PORT, () => {
+connectDB().then(() => {
+  app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
+  });
 });
